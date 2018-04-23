8fbce5e3c13da332b27bbf233f4a4c9c85ea9b09
hyg7778 <38646723+hyg7778@users.noreply.github.com>
2018-04-23 16:16:33 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,10 +39,10 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','bootcampmysql999.mysql.database.azure.com');
+define('DBHOST','mysql-handson0.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@bootcampmysql999');
-define('DBPASS','bootcamp@pass1');
+define('DBUSER','testuser@mysql-handson0');
+define('DBPASS','dudrms2965636!@');
 
 # Table prefix
 define('TABLE_PREFIX','ost_');
